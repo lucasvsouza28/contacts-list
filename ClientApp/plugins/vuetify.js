527b215db7ddeaec5480453d0d0c106e73796593
Lucas Souza <lucasvsouza28@gmail.com>
2019-11-01 09:58:36 -0300

    Update vuetify.js

--- ClientApp/plugins/vuetify.js
+++ ClientApp/plugins/vuetify.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-//import 'vuetify/dist/vuetify.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
 Vue.use(Vuetify);
 
